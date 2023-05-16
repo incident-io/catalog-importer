889fa588c1987eae8544abda9916793649316e1e
Lawrence Jones <lawrjone@gmail.com>
2023-05-16 14:07:28 +0100

    Backstage token signing
    When providing a token to the Backstage source, you can opt into
    generating a JWT from the provided token by setting signed to true.

--- source/source_backstage.go
+++ source/source_backstage.go
@@ -2,13 +2,16 @@ package source
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	kitlog "github.com/go-kit/kit/log"
 	"github.com/go-ozzo/ozzo-validation/is"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/golang-jwt/jwt"
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/pkg/errors"
 )
@@ -16,6 +19,7 @@ import (
 type SourceBackstage struct {
 	Endpoint string     `json:"endpoint"` // https://backstage.company.io/api/catalog/entities
 	Token    Credential `json:"token"`
+	Signed   bool       `json:"signed"`
 }
 
 func (s SourceBackstage) Validate() error {
@@ -32,6 +36,15 @@ func (s SourceBackstage) String() string {
 }
 
 func (s SourceBackstage) Load(ctx context.Context, logger kitlog.Logger) ([]*SourceEntry, error) {
+	token := string(s.Token)
+	if s.Signed {
+		var err error
+		token, err = s.getJWT()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	client := cleanhttp.DefaultClient()
 
 	var (
@@ -45,8 +58,8 @@ func (s SourceBackstage) Load(ctx context.Context, logger kitlog.Logger) ([]*Sou
 		if err != nil {
 			return nil, errors.Wrap(err, "building Backstage URL")
 		}
-		if string(s.Token) != "" {
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(s.Token)))
+		if token != "" {
+			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 		}
 
 		resp, err := client.Do(req)
@@ -75,4 +88,22 @@ func (s SourceBackstage) Load(ctx context.Context, logger kitlog.Logger) ([]*Sou
 
 		offset += len(page)
 	}
+}
+
+// getJWT applies the rules from the Backstage docs to generate a JWT that is valid for
+// external Backstage authentication.
+//
+// https://backstage.io/docs/auth/service-to-service-auth/#usage-in-external-callers
+func (s SourceBackstage) getJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = jwt.MapClaims{
+		"sub": "backstage-server",
+		"exp": time.Now().Add(time.Hour).Unix(),
+	}
+	secret, err := base64.StdEncoding.DecodeString(string(s.Token))
+	if err != nil {
+		return "", errors.Wrap(err, "supplied backstage token must be a base64 string")
+	}
+
+	return token.SignedString(secret)
 }
