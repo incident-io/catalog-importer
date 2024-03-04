f94d9f0d37ffe4358b725d3fe79f80f2bb5b0087
Liam Hegarty <liam.hegarty@incident.io>
2024-03-04 15:12:26 +0000

    strip out "clever" retry logic, get rate limit right

--- client/client.go
+++ client/client.go
@@ -2,25 +2,23 @@ package client
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
-	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/pkg/errors"
 	"golang.org/x/time/rate"
 )
 
-// last retry will wait 64 seconds (2^6)
+// last retry will wait 64 seconds
 // will also listen to 'Retry-After' header
+// 600 RPM, spread across 60 1 second buckets
 
 const (
-	rpm              = 600000000000000000
+	rpm              = 600
 	bucketsPerMinute = 60
 	maxRetries       = 7
 	minRetryWait     = 1 * time.Second
@@ -59,22 +57,6 @@ func attentiveBackoff(min, max time.Duration, attemptNum int, resp *http.Respons
 			}
 		}
 
-		// If not check if there is a response in the body
-		var target struct {
-			RateLimit struct {
-				RetryAfter string `json:"retry_after"`
-			} `json:"rate_limit"`
-		}
-		err := json.NewDecoder(resp.Body).Decode(&target)
-		spew.Dump(target)
-		if err == nil {
-			if target.RateLimit.RetryAfter != "" {
-				if retryAfterSeconds, err := time.ParseDuration(target.RateLimit.RetryAfter + "s"); err == nil {
-					return retryAfterSeconds
-				}
-			}
-		}
-
 		// otherwise use the default backoff
 	}
 	return retryablehttp.DefaultBackoff(min, max, attemptNum, resp)
@@ -97,13 +79,10 @@ func New(ctx context.Context, apiKey, apiEndpoint, version string, opts ...Clien
 
 	// The generated client won't turn validation errors into actual errors, so we do this
 	// // inside of a generic middleware.
-	base.Transport = Wrap(cleanhttp.DefaultTransport(), func(req *http.Request, next http.RoundTripper) (*http.Response, error) {
+	base.Transport = Wrap(base.Transport, func(req *http.Request, next http.RoundTripper) (*http.Response, error) {
 		resp, err := next.RoundTrip(req)
 		if err != nil {
 			return resp, err
-		}
-		if resp.StatusCode == http.StatusTooManyRequests {
-			return resp, nil
 		}
 		if err == nil && resp.StatusCode > 299 {
 			data, err := io.ReadAll(resp.Body)
@@ -119,7 +98,7 @@ func New(ctx context.Context, apiKey, apiEndpoint, version string, opts ...Clien
 
 	rlClient := &RateLimitedClient{
 		client:      base,
-		rateLimiter: rate.NewLimiter(rate.Every(time.Minute/bucketsPerMinute), rpm/bucketsPerMinute),
+		rateLimiter: rate.NewLimiter(rpm/60, 1),
 	}
 
 	clientOpts := append([]ClientOption{
